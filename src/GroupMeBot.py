3735434657818b07173eeffc295b588dd2f961dc
Keith <k3mell@gmail.com>
2018-10-07 15:21:37 -0400

    clean up

--- src/GroupMeBot.py
+++ src/GroupMeBot.py
@@ -4,16 +4,11 @@ from src import spreadsheet
 
 s_url = 'https://api.groupme.com/v3/groups/44100309/messages'  # smaller url
 rqst_prams = {'token': 'V34ln1DMe3q1yZPu8dGWnPhzzoPQxfY61CzNOwXO'}
-new_prams = {'token': 'V34ln1DMe3q1yZPu8dGWnPhzzoPQxfY61CzNOwXO',
-                      'after_id': '0'}
-
 resp_msgs = rqsts.get(s_url, params=rqst_prams).json()['response']['messages']
 # The 20 most recent messages, https://dev.groupme.com/docs/v3#messages
 
-new_msgs = rqsts.get(s_url, params=new_prams).json()['response']['messages']
-# recent messages, but using the param 'after_id'
-
-new_prams['after_id'] = resp_msgs[19]["id"]
+new_prams = {'token': 'V34ln1DMe3q1yZPu8dGWnPhzzoPQxfY61CzNOwXO',
+             'after_id': resp_msgs[19]["id"]}
 
 
 while 1:
@@ -30,7 +25,7 @@ while 1:
         # implement this command to reload the name conversion in the sheet.
         elif "event" in message:  # if msgs has 'event' then we care about it
             event = message["event"]
-            if "event.user" in message["event"]["type"]:  # only event responses have the substring 'event.user'
+            if "event.user" in event["type"]:  # only event responses have the substring 'event.user'
                 ans = event["type"]  # this should be the answer to the event, eg not_going.
                 nick = event["data"]["user"]["nickname"]  # this should be the GroupMe nickname
                 eventName = event["data"]["event"]["name"]  # this should be the GroupMe events name
